37f8a1fe0267010efd86d3af0ffbdd1e05c471b9
evangelisqa <albesaevan@icloud.com>
2025-01-19 20:31:26 -0800

    responsive button added

--- scripts/project.js
+++ scripts/project.js
@@ -1,33 +1,42 @@
-let currentIndex = 0;
 
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
+document.addEventListener('DOMContentLoaded', (Event)=>{
+    let currentIndex = 0;
 
-document.querySelector('.next').addEventListener('click', ()=>{
-    currentIndex = (currentIndex + 1) %totalSlides;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+document.querySelector('.next').addEventListener('click',()=>{
+    currentIndex = (currentIndex + 1) % totalSlides;
     updateSlider();
+    });
+    document.querySelector('.next').addEventListener('click',()=>{
+        currentIndex = (currentIndex + 1) % totalSlides;
+        updateSlider();
+    });
+    document.querySelector('.prev').addEventListener('click', ()=>{
+        currentIndex = (currentIndex -1 + totalSlides) % totalSlides;
+        updateSlider();
+    });
+
+    function updateSlider(){
+        const offset = -currentIndex * 100;
+        document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+    }
+
+    // auto slide
+    setInterval(()=> {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        updateSlider();
+    }, 3000);
+
+    // Update last modified time
+    const lastModifiedElement = document.getElementById('last-modified');
+    lastModifiedElement.textContent = document.lastModified;
+
+    //toggle menu
+    function toggleMenu(){
+        const header = document.querySelector('header');
+        header.classList.toggle('responsive');
+    }
 });
 
-document.querySelector('.prev').addEventListener('click',()=>{
-    currentIndex = (currentIndex - 1 + totalSlides)% totalSlides;
-    updateSlider();
-})
-
-function updateSlider(){
-    const offset = -currentIndex * 100;
-    document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
-}
-// Auto slide
-setInterval(()=>{
-    currentIndex = (currentIndex + 1) %totalSlides;
-    updateSlider();
-}, 3000);
-// Update last modified time
-const lastModifiedElement = document.getElementById('last-modified');
-lastModifiedElement.textContent = document.lastModified;
-
-// Toggle menu
-function toggleMenu(){
-    const header = document.querySelector('header');
-    header.classList.toggle('responsive');
-}
